c9caf076cd6d4f3ee721f95a606614b23cd2a962
Shereen <shereenfinney@gmail.com>
2016-12-07 12:19:04 +0000

    STRINGS - Convert a string to an array - 8kyu

---
+++ convert_string_to_array_8kyu.rb
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/convert-a-string-to-an-array/train/ruby
+
+def string_to_array(string)
+  #your code here
+string.split # test with: puts "This is #{string.split}"
+end
+
+#My Test
+string_to_array("Robin Singh") #Should be ["Robin", "Singh"]
