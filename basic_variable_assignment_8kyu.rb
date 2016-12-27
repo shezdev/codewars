7a6149372b16189aaf686d02a022bd8db13360d2
Shereen <shereenfinney@gmail.com>
2016-12-27 20:00:59 +0000

    VARIABLES - Basic variable assignment - 8kyu

---
+++ basic_variable_assignment_8kyu.rb
@@ -0,0 +1,6 @@
+# http://www.codewars.com/kata/50ee6b0bdeab583673000025/solutions/ruby
+
+
+a = "code"  #the == operator is a logical operator
+b = "wa.rs" #and therefore one needs to change it to =, which allows variables to be set.
+name = a + b 
